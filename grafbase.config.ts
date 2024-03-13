669eee092303cc3c896f54a585cf74b960128989
Lopeposkar <159769787+Lopeposkar@users.noreply.github.com>
2024-03-14 02:31:43 +0530

    Update grafbase.config.ts

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -9,6 +9,7 @@ const g = graph.Standalone()
 // Data Sources - https://grafbase.com/docs/connectors
 //
  const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
+ const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
  g.datasource(pg)
 
 // Resolvers - https://grafbase.com/docs/resolvers
@@ -23,7 +24,6 @@ export default config({
   // Authentication - https://grafbase.com/docs/auth
   auth: {
     // OpenID Connect
-     const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
      providers: [oidc],
     rules: (rules) => {
       rules.public()
